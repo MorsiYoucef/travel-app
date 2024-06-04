5ece5f2b7f8a355934944d3b3939be310c609404
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-06-04 12:03:20 +0100

    new commit

--- my-app/components/Camp.tsx
+++ my-app/components/Camp.tsx
@@ -1,3 +1,4 @@
+import { PEOPLE_URL } from '@/constants'
 import Image from 'next/image'
 
 interface CampProps {
@@ -22,6 +23,25 @@ const CampSite = ({
           <div className=" rounded-full bg-green-50 p-4">
             <Image src="/folded-map.svg" alt="map" width={28} height={28} />
           </div>
+          <div className=" flex flex-col gap-1">
+            <h4 className=" bold-18 text-white">{title}</h4>
+            <p className=" regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+        <div className=" flexCenter gap-6">
+          <span className=" flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image
+                className=" inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
+            ))}
+          </span>
+          <p className=" bold-16 md:bold-20 text-white">{peopleJoined}</p>
         </div>
       </div>
     </div>
@@ -30,7 +50,7 @@ const CampSite = ({
 
 const Camp = () => {
   return (
-    <section className=" border-2 border-green-50 2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20">
+    <section className="2xl:max-container relative flex flex-col py-10 lg:py-20 xl:mb-20">
       <div className=" hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
@@ -38,6 +58,32 @@ const Camp = () => {
           subtitle="Prigen, Pasuruan"
           peopleJoined="50+ Joined"
         />
+        <CampSite
+          backgroundImage="bg-bg-img-2"
+          title="Mountain view"
+          subtitle="Somewhere in the world"
+          peopleJoined="50+ Joined"
+        />
+      </div>
+      <div className=" flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className=" bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className=" regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling Lost</strong> And Not knowing The Way?
+          </h2>
+          <p className=" regular-14 xl:regular-16 mt-5 text-white">
+            {' '}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+            delectus vitae? Optio ea delectus amet dignissimos ipsa obcaecati ad
+            modi autem culpa expedita.
+          </p>
+          <Image
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className=" camp-quote"
+          />
+        </div>
       </div>
     </section>
   )
